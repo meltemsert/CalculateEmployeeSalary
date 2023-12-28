ccc40fe2735eef07392492931d1174ffa41a78aa
Meltem Sert <meltemsert29@gmail.com>
2023-12-28 18:12:11 +0300

    homework is done

---
+++ src/main/java/com/amigoscode/Main.java
@@ -0,0 +1,26 @@
+package com.amigoscode;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the day of employee please: ");
+        int countOfEmployeeDays = scanner.nextInt();
+        double totalSalary = calculateEmployeeSalary(countOfEmployeeDays);
+        System.out.println("Salary of Employee " + totalSalary + "TL");
+    }
+
+    private static double calculateEmployeeSalary(int countDay) {
+        double dailySalary = 1000.0;
+        if (countDay <= 25) {
+            return countDay * dailySalary;
+        } else {
+            double salary = countDay * dailySalary;
+            int extraDay = countDay - 25;
+            double prim = extraDay * dailySalary;
+            return salary + prim;
+        }
+
+    }
+}
